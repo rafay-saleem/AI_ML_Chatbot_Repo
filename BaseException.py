164facc8d55be521b79daf22eef4b8eb93f46fd4
rafay-saleem <rafayboss041@gmail.com>
2025-10-24 19:25:05 +0500

    Added Python Basic Practice (Day 1)

---
+++ BaseException.py
@@ -0,0 +1,12 @@
+
+print("Hello World")
+name="Rafay"
+age=19
+print(f"My name is", name, "I am", age, "years old")
+print("I'm king")
+identification=input("What's your name:")
+print("My name is:",identification)
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+result=num1+num2
+print("The sum is:",result,"Now yo're happy jan")
